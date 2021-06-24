aa9f0d77aaab9ecf55e0d89e3077abcdf3202d8b
Rodas20 <00006417@uca.edu.sv>
2021-06-24 04:58:40 -0600

    Adding LibrerySystem s Entities

---
+++ LibrarySystem.Entities/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.Entities
+{
+    public class Usuario
+    {
+		public int IdUsuario { get; set; }
+		public int Nombre { get; set; }
+		public string Apellido { get; set; }
+		public string Email { get; set; }
+		public string Contraseña { get; set; }
+		public int IdRol { get; set; }
+	}
+}
